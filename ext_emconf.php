50eec53a5e9953511d96d2d70b68babf29c3ccc2
jochen.rieger <jochen.rieger@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-04-16 16:36:46 +0000

    set version to 0.6.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cag_linkchecker/trunk@19067 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "cag_linkchecker"
 #
-# Auto generated 16-04-2009 16:41
+# Auto generated 16-04-2009 18:34
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'A backend module that lets you check your external links for validity. Originally developed for Connecta AG, Wiesbaden.',
 	'category' => 'module',
 	'shy' => 0,
-	'version' => '0.5.0',
+	'version' => '0.6.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -42,7 +42,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"bb21";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"3cd3";s:14:"ext_tables.php";s:4:"3c95";s:13:"locallang.php";s:4:"0be5";s:14:"doc/manual.sxw";s:4:"2e35";s:19:"doc/wizard_form.dat";s:4:"2485";s:20:"doc/wizard_form.html";s:4:"b4cb";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"a24b";s:14:"mod1/index.php";s:4:"952e";s:18:"mod1/locallang.php";s:4:"8563";s:22:"mod1/locallang_mod.php";s:4:"57d2";s:19:"mod1/moduleicon.gif";s:4:"7870";}',
+	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"37f4";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"3cd3";s:14:"ext_tables.php";s:4:"3c95";s:13:"locallang.php";s:4:"0be5";s:14:"doc/manual.sxw";s:4:"ed4b";s:19:"doc/wizard_form.dat";s:4:"2485";s:20:"doc/wizard_form.html";s:4:"b4cb";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"a24b";s:14:"mod1/index.php";s:4:"00c2";s:18:"mod1/locallang.php";s:4:"8563";s:22:"mod1/locallang_mod.php";s:4:"57d2";s:19:"mod1/moduleicon.gif";s:4:"7870";}',
 	'suggests' => array(
 	),
 );
